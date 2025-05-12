eca1e245f389fa4d9836530e8e00b84babc726d7
SnehaKoirala <345788@via.dk>
2025-05-13 00:59:29 +0200

    Fix routing issue for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
-import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import CharacterTable from "./pages/CharacterTable";
-import Navbar from "./components/Navbar";
-
+import Navbar from "./components/Navbar"; // Import Navbar component
 import "./App.css";
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <Navbar />
+    <Router basename="/Rick_And_Morty">  {/* Use the correct basename */}
+      <Navbar />  {/* Use Navbar */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/characters" element={<CharacterTable />} />
